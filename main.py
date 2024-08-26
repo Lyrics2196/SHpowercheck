4c6b018c57e6fa4005a8b24c15b7dee4431218c0
Lyrics2196 <linyr2196@outlook.com>
2024-08-26 21:07:44 +0800

    rewrite

--- main.py
+++ main.py
@@ -1,60 +1,41 @@
 import requests
-import json
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+# 基本信息设置
+url = "https://shgy.buaa.edu.cn/ics/rest/wxdev/getlvalue"  # 查询电量的API链接
+student_id = "xxxxxxxxxxxx"  # 学号
+receiver_email = [
+    "email1@qq.com",
+    "email2@buaa.edu.cn",
+]  # 收件人邮箱，多个邮箱用列表表示
+sender_email = "xxxxxx"  # 发送通知的邮箱
+password = "xxxxxxxxxxxxxxx"  # 邮箱的SMTP授权码
+smtp_server = "smtp.xx.com"  # SMTP服务器地址
+smtp_port = 587  # SMTP端口，默认587，163邮箱使用465
+threshold = 100  # 电量阈值，低于此值发送警告邮件
 
-def get_balance():
-    url = "https://shgy.buaa.edu.cn/ics/rest/wxdev/getlvalue"
+# 获取电量信息
+headers = {"Content-Type": "application/json; charset=UTF-8"}
+json_data = {"stucode": student_id, "type": 1}
+response = requests.post(url, headers=headers, json=json_data)
+provalue = response.json()["data"]["provalue"]
+balance = int(provalue[:-3])
 
-    headers = {
-        "Content-Type": "application/json; charset=UTF-8",
-    }
+# 发送警告邮件
+message = MIMEMultipart()
+message["From"] = sender_email
+message["To"] = ", ".join(receiver_email)
+message["Subject"] = "Warning"
+body = f"The available power is insufficient. Remaining power: {balance} kWh"
+message.attach(MIMEText(body, "plain"))
 
-    json_data = {
-        "stucode": "xxxxxxxx",
-        "type": 1,
-    }
+smtp = smtplib.SMTP(smtp_server, smtp_port)
+smtp.starttls()
+smtp.login(sender_email, password)
 
-    response = requests.post(
-        url=url,
-        headers=headers,
-        json=json_data,
-    )
+if balance < threshold:
+    smtp.sendmail(sender_email, receiver_email, message.as_string())
 
-    data = json.loads(response.text)
-    provalue = data["data"]["provalue"]
-    balance = int(provalue[:-3])
-
-    return balance
-
-
-def send_email(balance, receiver_email):
-    sender_email = "xxxxxxxxxxxxx"
-    password = "xxxxxxxxxxxxxxxxxxxx"
-
-    message = MIMEMultipart()
-    message["From"] = sender_email
-    message["To"] = receiver_email
-    message["Subject"] = "宿舍可用电量不足提醒"
-
-    message.attach(
-        MIMEText(
-            "宿舍可用电量不足，请及时充值\n 目前可用电量：" + str(balance) + "度",
-            "plain",
-        )
-    )
-    with smtplib.SMTP("smtp.qq.com", 587) as server:
-        server.starttls()
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message.as_string())
-
-
-if __name__ == "__main__":
-    receiver_email1 = "xxxxxxxxxxx@buaa.edu.cn"
-    receiver_email2 = "xxxxxxxxxxxxxx@qq.com"
-    balance = get_balance()
-    if balance < 5:
-        send_email(balance, receiver_email1)
-        send_email(balance, receiver_email2)
+smtp.quit()
