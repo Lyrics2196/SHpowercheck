bbf42ca90118cebd519892ce4fdc2c19c6bd9123
Lyrics2196 <linyr2196@outlook.com>
2024-08-28 11:37:37 +0800

    add logger

--- power.py
+++ power.py
@@ -1,5 +1,7 @@
 import requests
 import smtplib
+import logging
+import sys
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
@@ -16,26 +18,44 @@ smtp_server = "smtp.xx.com"  # SMTP服务器地址
 smtp_port = 587  # SMTP端口，默认587，163邮箱使用465
 threshold = 100  # 电量阈值，低于此值发送警告邮件
 
+
 # 获取电量信息
-headers = {"Content-Type": "application/json; charset=UTF-8"}
-json_data = {"stucode": student_id, "type": 1}
-response = requests.post(url, headers=headers, json=json_data)
-provalue = response.json()["data"]["provalue"]
-balance = int(provalue[:-3])
+def get_power():
+    headers = {"Content-Type": "application/json; charset=UTF-8"}
+    json_data = {"stucode": student_id, "type": 1}
+    response = requests.post(url, headers=headers, json=json_data)
+    provalue = response.json()["data"]["provalue"]
+    balance = int(provalue[:-3])
+    return balance
+
 
 # 发送警告邮件
-message = MIMEMultipart()
-message["From"] = sender_email
-message["To"] = ", ".join(receiver_email)
-message["Subject"] = "宿舍电量警告"
-body = f"宿舍可用电费不足，请及时充值。电量余额: {balance} kWh"
-message.attach(MIMEText(body, "plain"))
+def send_email(balance):
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = ", ".join(receiver_email)
+    message["Subject"] = "宿舍电量警告"
+    body = f"宿舍可用电费不足，请及时充值。电量余额: {balance} kWh"
+    message.attach(MIMEText(body, "plain"))
 
-smtp = smtplib.SMTP(smtp_server, smtp_port)
-smtp.starttls()
-smtp.login(sender_email, password)
-
-if balance < threshold:
+    smtp = smtplib.SMTP(smtp_server, smtp_port)
+    smtp.starttls()
+    smtp.login(sender_email, password)
     smtp.sendmail(sender_email, receiver_email, message.as_string())
+    smtp.quit()
 
-smtp.quit()
+
+if __name__ == "__main__":
+    # 配置日志
+    logging.basicConfig(
+        filename=sys.path[0] + "\\power.log",
+        level=logging.INFO,
+        filemode="a",
+        format="%(asctime)s - %(levelname)s - %(message)s",
+    )
+    balance = get_power()
+    if balance < threshold:
+        send_email(balance)
+        logging.info(f"电量余额{balance}kWh，低于阈值{threshold}kWh，发送警告邮件")
+    else:
+        logging.info(f"电量余额{balance}kWh，高于阈值{threshold}kWh，无需发送警告邮件")
